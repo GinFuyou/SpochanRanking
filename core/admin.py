951552c986a3457fe10c98c32c7e2a3e323fee3b
GinFuyou <webmaster@doratoa.net>
2022-10-30 20:36:20 +0300

    updated configs, fix admin fields

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,33 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 
 @admin.register(get_user_model())
 class CoreUserAdmin(UserAdmin):
     list_display = ['admin_display_name', 'email', 'is_staff', 'is_active', 'date_joined']
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    ("is_staff", "is_superuser"),
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
