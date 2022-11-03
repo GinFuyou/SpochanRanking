f5f5c58dc207667a8a804c91fb6f27b792ba5d39
GinFuyou <webmaster@doratoa.net>
2022-11-03 19:43:54 +0300

    drafts of competition models

--- chanbara/models.py
+++ chanbara/models.py
@@ -21,3 +21,41 @@ class SportDiscipline(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Competition(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    name = models.CharField(max_length=512)
+    slug = models.SlugField(max_length=256, unique=True)
+    scale = models.DecimalField(max_digits=9, decimal_places=4)
+
+    date = models.DateField()
+    # location = ?
+
+
+class CompGroup(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=('competition', 'name'), name='unique_group_name_for_competition'),
+        ]
+
+    competition = models.ForeignKey('Competition')
+    name = models.CharField(max_length=256)
+    # grand_champion = models.ForeignKey(....)
+
+
+"""
+class CompGroupDiscipline(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=('group', 'discipline'), name='unique_discipline_for_group'),
+        ]
+
+    group = models.ForeignKey("CompGroup")
+    discipline = models.ForeignKey("SportDiscipline")
+    # place1 = models.ForeignKey(....)
+    # place2 = models.ForeignKey(....)
+    # place3 = models.ForeignKey(....)
+    # place4 = models.ForeignKey(....)
+"""
